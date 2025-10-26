4c5bd0828c6788cc6d3a58d875107344762572b7
PrathameshBirajdar <prathameshbirajdar.git@gmail.com>
2025-10-27 05:05:21 +0530

    updated chat

--- chat/apps.py
+++ chat/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 class ChatConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "aibuddy.chat"  # ✅ must match import path exactly
+    name = "aibuddy.chat"  # ✅ full path
     verbose_name = "AI Buddy Chat"
